cf6808c3763d72b3405745fcc196caab473b8d55
Arturs Drozdovs <arturs.drozdovs@teampicnic.com>
2019-12-01 22:06:25 +0100

    Enable eslint config / Update gitignore

---
+++ web/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "plugin:vue/recommended",
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+    "rules": {
+        "semi": [2, "always"]
+    }
+};
